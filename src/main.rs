eae377f259cd87389ce999a173951ceea5de1a3a
kosinix <kosinix@users.noreply.github.com>
2016-12-17 20:50:06 +0800

    Update commands

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,14 @@ Raster CLI Commands.
 
 Usage:
   raster-cli resize <src> <dest> [--rmode=<m>]
+  raster-cli crop <src> <dest> <width> <height> [ --pos=<p> <offx> <offy> ]
+  raster-cli rotate <src> <dest> <degrees>
   raster-cli (-h | --help)
   raster-cli (-v | --version)
 
 Options:
     --rmode=<m>         Resize mode (fit|fill|exact|exact_width|exact_height) [default: fit].
+    --pos=<p>           Position (top-left|top-center|top-right|center-left|center|center-right|bottom-left|bottom-center|bottom-right) [default: center].
     -h --help          Show this screen.
     -v --version       Show version.
 ";
@@ -23,6 +26,11 @@ struct Args {
     cmd_resize: bool,
     arg_src: Option<String>,
     arg_dest: Option<String>,
+    arg_width: Option<u32>,
+    arg_height: Option<u32>,
+    arg_offx: Option<i32>,
+    arg_offy: Option<i32>,
+    flag_pos: String,
     flag_rmode: String,
     flag_help: bool,
     flag_version: bool,
@@ -32,7 +40,7 @@ fn main() {
     let args: Args = Docopt::new(USAGE)
                             .and_then(|d| d.decode())
                             .unwrap_or_else(|e| e.exit());
-    // println!("{:?}", args);
+    println!("{:?}", args);
     
 
     if args.cmd_resize {
