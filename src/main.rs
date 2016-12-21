993bd47fcef7f4066aceaab519b77e92497c6e9c
kosinix <kosinix@users.noreply.github.com>
2016-12-21 19:08:36 +0800

    Add more commands and version constants

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,20 @@ extern crate rustc_serialize;
 
 use docopt::Docopt;
 
+const VERSION: &'static str = "0.1.0";
+const RASTER: &'static str = "0.0.8";
 const USAGE: &'static str = "
 Raster CLI Commands.
 
 Usage:
-  raster-cli resize <src> <dest> [--rmode=<m>]
-  raster-cli crop <src> <dest> <width> <height> [ --pos=<p> <offx> <offy> ]
-  raster-cli rotate <src> <dest> <degrees>
+  raster-cli resize <src> <dest> <width> <height> [ <mode> ]
+  raster-cli crop <src> <dest> <width> <height> [ <pos> <offx> <offy> ]
+  raster-cli rotate <src> <dest> <degrees> [ <bg_color> ]
   raster-cli (-h | --help)
   raster-cli (-v | --version)
 
 Options:
-    --rmode=<m>         Resize mode (fit|fill|exact|exact_width|exact_height) [default: fit].
-    --pos=<p>           Position (top-left|top-center|top-right|center-left|center|center-right|bottom-left|bottom-center|bottom-right) [default: center].
+-j --json              Return result as JSON.
     -h --help          Show this screen.
     -v --version       Show version.
 ";
@@ -24,14 +25,19 @@ Options:
 #[derive(Debug, RustcDecodable)]
 struct Args {
     cmd_resize: bool,
+    cmd_crop: bool,
+    cmd_rotate: bool,
     arg_src: Option<String>,
     arg_dest: Option<String>,
-    arg_width: Option<u32>,
-    arg_height: Option<u32>,
+    arg_width: Option<i32>,
+    arg_height: Option<i32>,
+    arg_mode: Option<String>,
+    arg_pos: Option<String>,
     arg_offx: Option<i32>,
     arg_offy: Option<i32>,
-    flag_pos: String,
-    flag_rmode: String,
+    arg_degrees: Option<i32>,
+    arg_bg_color: Option<String>,
+    flag_json: bool,
     flag_help: bool,
     flag_version: bool,
 }
@@ -44,20 +50,68 @@ fn main() {
     
 
     if args.cmd_resize {
-        println!("resizing... ");
-        let mut image = raster::open(args.arg_src.unwrap().as_str()).unwrap();
-        match raster::editor::resize(&mut image, 150, 150, args.flag_rmode.as_str()) {
+        // TODO: sanity checks
+        println!("Resizing image ... ");
+        println!(" ");
+        let src = args.arg_src.unwrap();
+        let dest = args.arg_dest.unwrap();
+        let resize_w = args.arg_width.unwrap();
+        let resize_h = args.arg_height.unwrap();
+        let mode = args.arg_mode.unwrap_or("fit".to_string());
+        
+        let mut image = raster::open(src.as_str()).unwrap();
+        match raster::editor::resize(&mut image, resize_w, resize_h, mode.as_str()) {
             Ok(_) => {
-                raster::save(&image, args.arg_dest.unwrap().as_str());
-                println!("success");
+                raster::save(&image, dest.as_str());
+                println!("Done!");
             },
             Err(s) => {
-                println!("error: {}", s);
+                println!("Error: {}", s);
             }
         }
-        
+    } else if args.cmd_crop {
+        // TODO: sanity checks
+        println!("Cropping image ... ");
+        println!(" ");
+        let src = args.arg_src.unwrap();
+        let dest = args.arg_dest.unwrap();
+        let crop_w = args.arg_width.unwrap();
+        let crop_h = args.arg_height.unwrap();
+        // optionals
+        let pos = args.arg_pos.unwrap_or("center".to_string());
+        let offx = args.arg_offx.unwrap_or(0);
+        let offy = args.arg_offy.unwrap_or(0);
+
+        let mut image = raster::open(src.as_str()).unwrap();
+        match raster::editor::crop(&mut image, crop_w, crop_h, pos.as_str(), offx, offy) {
+            Ok(_) => {
+                raster::save(&image, dest.as_str());
+                println!("Done!");
+            },
+            Err(s) => {
+                println!("Error: {}", s);
+            }
+        }
+    } else if args.cmd_rotate {
+        println!("Rotating image ... ");
+        println!(" ");
+        let src = args.arg_src.unwrap();
+        let dest = args.arg_dest.unwrap();
+        let degrees = args.arg_degrees.unwrap();
+        let bg_color = args.arg_bg_color.unwrap_or("#000000".to_string());
+        let bg_color = raster::Color::hex(bg_color.as_str()).unwrap();
+        let mut image = raster::open(src.as_str()).unwrap();
+        match raster::transform::rotate(&mut image, degrees, bg_color) {
+            Ok(_) => {
+                raster::save(&image, dest.as_str());
+                println!("Done!");
+            },
+            Err(s) => {
+                println!("Error: {}", s);
+            }
+        }
     } else if args.flag_version {
-        println!("raster-cli v0.1.0");
-        println!("raster v0.0.7");
+        println!("raster-cli v{}", VERSION);
+        println!("raster v{}", RASTER);
     }
 }
